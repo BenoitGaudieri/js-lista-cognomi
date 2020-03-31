47a59117c58606f88e601362795a3d4d381965cd
BenoitGaudieri <benalek@gmail.com>
2020-03-31 13:38:12 +0200

    feat. logic completed

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// References
+var newSurname = document.getElementById("new-surname");
+var surnameList = document.getElementById("surname-list");
+var btnAdd = document.getElementById("add");
+var list = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+
+// Event Listener
+btnAdd.addEventListener("click", function() {
+    // aggiunta str alla lista
+    list.push(newSurname.value);
+
+    // reference di str vuota per output
+    var items = "";
+
+    // Sort list
+    list.sort();
+    console.log(list);
+
+    // Index Of
+    console.log(list.indexOf(newSurname.value));
+
+    // Aggiunta alla str d'output
+    for (var i = 0; i < list.length; i++) {
+        items += "<li>" + list[i] + "</li>";
+    }
+
+    // output
+    surnameList.innerHTML = items;
+
+    // reset
+    newSurname.value = "";
+    newSurname.focus();
+});
